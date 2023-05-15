52df5b406303aba47919fc4c43b340f5361b725f
Matt Rice <matthewcrice32@gmail.com>
2023-05-14 22:55:03 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@uma/core/oracle/implementation/Voting2.sol";
+
+contract Delegate is ERC20, Ownable {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
